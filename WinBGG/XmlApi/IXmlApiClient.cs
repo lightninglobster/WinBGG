0c18f30cc6e4460f751ba9a4b952378d0849e33b
Frank Olszewski <algorhythm@gmail.com>
2016-03-26 11:25:49 -0400

    Test Push Please Ignore

---
+++ WinBGG/XmlApi/IXmlApiClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinBGG.XmlApi
+{
+    interface IXmlApiClient
+    {
+        #region Properties
+        bool IsValid { get; }
+        bool Status { get; }
+        #endregion Properties
+
+        #region Public Methods
+        bool Connect();
+        bool Disconnect();
+        #endregion Public Methods
+    }
+}
---
+++ WinBGG/XmlApi/XmlApiClient.cs
@@ -0,0 +1,40 @@
+﻿namespace WinBGG.XmlApi
+{
+    public class XmlApiClient : IXmlApiClient
+    {
+        #region MVs
+        protected bool _connected;
+        protected bool _valid;
+        #endregion MVs
+
+        #region Properties
+        public bool IsValid
+        {
+            get { return _valid; }
+        }
+        public bool Status
+        {
+            get { return _connected; }
+        }
+        #endregion Properties
+
+        #region Constructors
+        public XmlApiClient()
+        {
+            _connected = false;
+            _valid = true;
+        }
+        #endregion Constructors
+
+        #region Public Methods
+        public bool Connect()
+        {
+            return (_connected = true);
+        }
+        public bool Disconnect()
+        {
+            return (_connected = false);
+        }
+        #endregion Public Methods
+    }
+}
